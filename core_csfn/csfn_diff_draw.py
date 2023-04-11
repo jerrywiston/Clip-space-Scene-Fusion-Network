c625df66cd2447bb9cb4410b45c742b0fcac7dc5
jerrywiston <dreamfantasy0@gmail.com>
2023-04-11 20:30:15 +0800

    added diffusion refinement.

---
+++ core_csfn/csfn_diff_draw.py
@@ -0,0 +1,39 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import encoder
+#import decoder_vae as decoder
+#import decoder_diff as decoder
+import decoder_diff_draw as decoder
+import cstrn
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Clip Space Scene Fusion Network
+class CSFN(nn.Module):
+    def __init__(self, view_size=(16,16), depth_size=6, pose_size=12, emb_size=32, cell_size=128, fusion_type="ocm"):
+        super(CSFN, self).__init__()
+        # Set Variables
+        self.view_size = view_size
+        self.depth_size = depth_size
+        self.pose_size = pose_size
+        self.emb_size = emb_size
+        self.cell_size = cell_size
+        self.fusion_type = fusion_type
+
+        # Build Modules
+        self.encoder = encoder.EncoderNetworkRes(csize=cell_size).to(device)
+        self.cstrn = cstrn.CSTRN(view_size, depth_size, pose_size, emb_size, cell_size, fusion_type)
+        self.renderer = decoder.Renderer(cell_size=cell_size, z_dim=64).to(device)
+
+    def forward(self, xo, xq, pose_o, pose_q):
+        view_cell_o = self.encoder(xo)
+        view_cell_q_layers = self.cstrn(view_cell_o, pose_o, pose_q)
+        kl_loss, rec_loss, draw_loss, diff_loss, x_rec = self.renderer.get_loss(xq, view_cell_q_layers)
+        return kl_loss, rec_loss, draw_loss, diff_loss, x_rec
+    
+    def sample(self, xo, pose_o, pose_q):
+        view_cell_o = self.encoder(xo)
+        view_cell_q_layers = self.cstrn(view_cell_o, pose_o, pose_q)
+        x_samp_draw, x_samp_diff = self.renderer.sample(view_cell_q_layers)
+        return x_samp_draw, x_samp_diff
--- diffusion/model_refine.py
+++ diffusion/model_refine.py
@@ -15,7 +15,7 @@ class DiffusionModel(nn.Module):
         self.device = device
 
         bilinear = True
-        self.inc = DoubleConv(img_depth, 64)
+        self.inc = DoubleConv(img_depth*2, 64)
         self.down1 = Down(64+cell_size, 128) #(64,64)->(32,32)
         self.down2 = Down(128+cell_size, 256) #(32,32)->(16,16)
         factor = 2 if bilinear else 1
@@ -43,14 +43,14 @@ class DiffusionModel(nn.Module):
         pos_enc = torch.cat([pos_enc_a, pos_enc_b], dim=-1)
         return pos_enc.view(-1, channels, 1, 1).repeat(1, 1, embed_size, embed_size)
 
-    def forward(self, x, c, t):
+    def forward(self, x, xd, c, t):
         """
         Model is U-Net with added positional encodings and self-attention layers.
         """
         img_size = int(self.in_size ** (1/2))
 
         # Down
-        x1 = self.inc(x)
+        x1 = self.inc(torch.cat((x, xd), 1))
         x1_ = torch.cat((x1, self.up4x4(c)), 1)
 
         x2 = self.down1(x1_) + self.pos_encoding(t, 128, int(img_size/2))
@@ -76,24 +76,6 @@ class DiffusionModel(nn.Module):
         output = self.outc(x7_)
         return output
 
-    def forward_no_cond(self, x, t):
-        """
-        Model is U-Net with added positional encodings and self-attention layers.
-        """
-        img_size = int(self.in_size ** (1/2))
-        x1 = self.inc(x)
-        x2 = self.down1(x1) + self.pos_encoding(t, 128, int(img_size/2))
-        x3 = self.down2(x2) + self.pos_encoding(t, 256, int(img_size/4))
-        x3 = self.sa1(x3)
-        x4 = self.down3(x3) + self.pos_encoding(t, 256, int(img_size/8))
-        x4 = self.sa2(x4)
-        x = self.up1(x4, x3) + self.pos_encoding(t, 128, int(img_size/4))
-        x = self.sa3(x)
-        x = self.up2(x, x2) + self.pos_encoding(t, 64, int(img_size/2))
-        x = self.up3(x, x1) + self.pos_encoding(t, 64, int(img_size))
-        output = self.outc(x)
-        return output
-
     def beta(self, t):
         return self.beta_small + (t / self.t_range) * (
             self.beta_large - self.beta_small
@@ -105,27 +87,27 @@ class DiffusionModel(nn.Module):
     def alpha_bar(self, t):
         return math.prod([self.alpha(j) for j in range(t)])
 
-    def get_loss(self, batch, c):
+    def get_loss(self, x, xd, c):
         """
         Corresponds to Algorithm 1 from (Ho et al., 2020).
         """
-        ts = torch.randint(0, self.t_range, [batch.shape[0]], device=self.device)
+        ts = torch.randint(0, self.t_range, [x.shape[0]], device=self.device)
         noise_imgs = []
-        epsilons = torch.randn(batch.shape, device=self.device)
+        epsilons = torch.randn(x.shape, device=self.device)
         
         for i in range(len(ts)):
             a_hat = self.alpha_bar(ts[i])
             noise_imgs.append(
-                (math.sqrt(a_hat) * batch[i]) + (math.sqrt(1 - a_hat) * epsilons[i])
+                (math.sqrt(a_hat) * x[i]) + (math.sqrt(1 - a_hat) * epsilons[i])
             )
         noise_imgs = torch.stack(noise_imgs, dim=0)
-        e_hat = self.forward(noise_imgs, c, ts.unsqueeze(-1).type(torch.float))
+        e_hat = self.forward(noise_imgs, xd, c, ts.unsqueeze(-1).type(torch.float))
         loss = nn.functional.mse_loss(
             e_hat.reshape(-1, self.in_size), epsilons.reshape(-1, self.in_size)
         )
         return loss
 
-    def denoise_sample(self, x, c, t):
+    def denoise_sample(self, x, xd, c, t):
         """
         Corresponds to the inner loop of Algorithm 2 from (Ho et al., 2020).
         """
@@ -134,29 +116,21 @@ class DiffusionModel(nn.Module):
                 z = torch.randn(x.shape).to(self.device)
             else:
                 z = 0
-            e_hat = self.forward(x, c, t.view(1, 1).repeat(x.shape[0], 1))
+            e_hat = self.forward(x, xd, c, t.view(1, 1).repeat(x.shape[0], 1))
             pre_scale = 1 / math.sqrt(self.alpha(t))
             e_scale = (1 - self.alpha(t)) / math.sqrt(1 - self.alpha_bar(t))
             post_sigma = math.sqrt(self.beta(t)) * z
             x = pre_scale * (x - e_scale * e_hat) + post_sigma
             return x
 
-    def image_sample(self, c, batch_size):
+    def image_sample(self, xd, c, batch_size):
         x = torch.randn((batch_size, 3, 64, 64)).to(self.device)
         sample_steps = torch.arange(self.t_range-1, 0, -1).to(self.device)
         for t in sample_steps:
-            x = self.denoise_sample(x, c, t)
+            x = self.denoise_sample(x, xd, c, t)
         #x = (x.clamp(-1,1) + 1) / 2
         x = x.clamp(0,1)
         return x
-
-    def training_step(self, batch, batch_idx):
-        loss = self.get_loss(batch, batch_idx)
-        return loss
-
-    def validation_step(self, batch, batch_idx):
-        loss = self.get_loss(batch, batch_idx)
-        return loss
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=2e-4)
